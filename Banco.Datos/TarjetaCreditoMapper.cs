b77a442db5bb3ad5b962c1369dff1a947d53cd3c
Augustojf98 <57848420+Augustojf98@users.noreply.github.com>
2020-11-27 20:14:10 -0300

    TarjetaCreditoMapper y TarjetaCredito

--- Banco.Datos/TarjetaCreditoMapper.cs
+++ Banco.Datos/TarjetaCreditoMapper.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Configuration;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Banco.Entidades;
+using Newtonsoft.Json;
 
 namespace Banco.Datos
 {
-    class TarjetaCreditoMapper
+    public class TarjetaCreditoMapper
     {
+        public List<TarjetaCredito> TraerTodos()
+        {
+            string json2 = WebHelper.Get("/tarjetacredito/889059");
+            List<TarjetaCredito> lst = MapList(json2);
+            return lst;
+        }
+
+        public List<TarjetaCredito> MapList(string json2)
+        {
+            List<TarjetaCredito> resultado = JsonConvert.DeserializeObject<List<TarjetaCredito>>(json2);
+            return resultado;
+        }
+
+        public TransactionResult MapResultado(string json)
+        {
+            TransactionResult result = JsonConvert.DeserializeObject<TransactionResult>(json);
+            return result;
+        }
+
+        public TransactionResult InsertTC(TarjetaCredito tarjetaCredito)
+        {
+            NameValueCollection obj = ReverseMap(tarjetaCredito);
+
+            string result = WebHelper.Post("/tarjetacredito", obj);
+
+            TransactionResult result2 = MapResultado(result);
+
+            return result2;
+        }
+
+        public NameValueCollection ReverseMap(TarjetaCredito tarjetaCredito)
+        {
+            NameValueCollection collection = new NameValueCollection();
+            collection.Add("Usuario", ConfigurationManager.AppSettings["Legajo"].ToString());
+            collection.Add("idCliente", tarjetaCredito.IdCliente.ToString());
+            collection.Add("Tipo", tarjetaCredito.Tipo.ToString());
+            collection.Add("PeriodoVencimiento", tarjetaCredito.PeriodoVencimiento.ToString());
+            collection.Add("LimiteCompra", tarjetaCredito.LimiteCompra.ToString());
+            collection.Add("NroPlastico", tarjetaCredito.NroPlastico.ToString());
+
+            return collection;
+        }
+
     }
 }
--- Banco.Entidades/TarjetaCredito.cs
+++ Banco.Entidades/TarjetaCredito.cs
@@ -3,10 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
 
 namespace Banco.Entidades
 {
+    [DataContract]
     public class TarjetaCredito
     {
+        private int _idCliente;
+        private int _periodoVencimiento;
+        private int _tipo;
+        private double _limiteCompra;
+        private string _nroPlastico;
+
+        [DataMember]
+        public string NroPlastico
+        {
+            get
+            {
+                return this._nroPlastico;
+            }
+            set
+            {
+                this._nroPlastico = value;
+            }
+        }
+
+        [DataMember]
+        public int Tipo
+        {
+            get
+            {
+                return this._tipo;
+            }
+            set
+            {
+                this._tipo = value;
+            }
+        }
+
+        [DataMember]
+        public int IdCliente
+        {
+            get
+            {
+                return this._idCliente;
+            }
+            set
+            {
+                this._idCliente = value;
+            }
+        }
+
+        [DataMember]
+        public int PeriodoVencimiento
+        {
+            get
+            {
+                return this._periodoVencimiento;
+            }
+            set
+            {
+                this._periodoVencimiento = value;
+            }
+        }
+
+        [DataMember]
+        public double LimiteCompra
+        {
+            get
+            {
+                return this._limiteCompra;
+            }
+            set
+            {
+                this._limiteCompra = value;
+            }
+        }
+
+        public TarjetaCredito() {}
+
+        public TarjetaCredito(int idCliente, int tipo, int periodoVencimiento, double limiteCompra, string nroPlastico)
+        {
+            this._tipo = tipo;
+            this._periodoVencimiento = periodoVencimiento;
+            this._idCliente = idCliente;
+            this._limiteCompra = limiteCompra;
+            this._nroPlastico = nroPlastico;
+        }
     }
 }
